0c2efbccf1edb3fb45f8477d9644c3fb68506970
rajeshThappeta <33081925+rajeshThappeta@users.noreply.github.com>
2025-12-05 11:12:07 +0530

    Update registration form submission to async/await
    Refactor form submission to use async/await for HTTP request and handle errors.

--- Register.jsx
+++ Register.jsx
@@ -1,4 +1,7 @@
 import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Register() {
   const {
@@ -7,15 +10,38 @@ function Register() {
     formState: { errors },
   } = useForm();
 
+  let [registerErr, setRegisterErr] = useState("");
+
+  const navigate = useNavigate();
+
   //function to form submit
-  const onFormSubmit = (newUser) => {
+  const onFormSubmit = async (newUser) => {
     newUser.todos = [];
     console.log(newUser);
+    try {
+      //Make HTTP POST req tio create new User in Backend
+      let res = await axios.post("http://localhost:8000/user-api/user", newUser);
+      console.log("res is ", res);
+      //if resourse is created
+      if (res.status === 201) {
+        //naviagte to login component programatically
+        navigate("/login");
+      } else {
+        //display error message
+        console.log(res.data.message);
+      }
+    } catch (err) {
+      console.log("err is ", err.response.data.message);
+      setRegisterErr(err.response.data.message);
+    }
   };
 
   return (
     <div>
       <h1 className="text-center text-info display-3">User Registration</h1>
+
+      {/* display registration error message */}
+      {registerErr.length !== 0 && <p className="fs-3 text-warning text-center">{registerErr}</p>}
       {/* registration form */}
       <form className="w-50 mx-auto mt-5" onSubmit={handleSubmit(onFormSubmit)}>
         <div className="mb-3">
